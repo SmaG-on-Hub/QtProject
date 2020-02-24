27ee9597229a8e0c96aca825d87036194eb0fc25
SmaG <smag@ua.fm>
2020-02-24 03:43:15 +0200

    Ready project, written on QThreadPool and QRunnable

---
+++ Delta_test/qcopyworker.cpp
@@ -0,0 +1,6 @@
+#include "qcopyworker.h"
+
+QCopyWorker::QCopyWorker()
+{
+
+}
---
+++ Delta_test/qcopyworker.h
@@ -0,0 +1,11 @@
+#ifndef QCOPYWORKER_H
+#define QCOPYWORKER_H
+
+
+class QCopyWorker : public QRunnable
+{
+public:
+    QCopyWorker();
+};
+
+#endif // QCOPYWORKER_H
