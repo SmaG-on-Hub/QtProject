a4e537b59b53a5f20aed3264e912e04f512df611
SmaG <smag@ua.fm>
2020-02-26 03:58:33 +0200

    Update

--- Delta_test/main.cpp
+++ Delta_test/main.cpp
@@ -1,8 +1,48 @@
+#include "qcopyworker.h"
+
+#include "toml11/toml.hpp"
+
 #include <QCoreApplication>
+#include <QThreadPool>
+#include <list>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+/*    std::ofstream test_file("first.dat", std::ios_base::binary);
+    char buff[1048576];
+
+    for (int var = 0; var < 1000; ++var) {
+        test_file.write(buff, 1048576);
+    }
+
+    test_file.close();*/
+
+
+        QThreadPool workersPool;
+        std::ifstream ifs("Files_to_copy.toml", std::ios_base::binary);
+
+        if (not ifs.good())
+            std::cout<< "Nope!";
+
+        const auto data = toml::parse(ifs);
+        //const auto tdata = toml::find(data, "files");
+        const int max_threads = toml::find_or<int>(data, "max_threads", 2);
+        const auto dist = toml::find_or<std::string>(data, "dist", "");
+        auto files = toml::get<std::list<std::string>>(toml::find(data, "files"));
+
+        workersPool.setMaxThreadCount(max_threads);
+
+        QCopyWorker* temp;
+        while(!files.empty())
+        {
+            temp = new QCopyWorker(files.front(), dist);
+            workersPool.start(temp, QThread::Priority::NormalPriority);
+            files.pop_front();
+        }
+
+        ifs.close();
 
     return a.exec();
 }
--- Delta_test/qcopyworker.cpp
+++ Delta_test/qcopyworker.cpp
@@ -1,6 +1,38 @@
 #include "qcopyworker.h"
+#include <QtTest/QTest>
 
-QCopyWorker::QCopyWorker()
+
+QCopyWorker::QCopyWorker(std::string path, std::string dist_path) : path_(path), dist_path_(dist_path)
 {
+    file_name_ = path_.substr(path_.find_last_of("\\")+1);
+    std::cout<< file_name_<< " is in process.." << std::endl;
+}
+
+QCopyWorker::~QCopyWorker()
+{
+    in_file_.close();
+    out_file_.close();
+    long long elapsed_time = std::chrono::duration_cast<std::chrono::seconds>(t_end_point_ - t_st_point_).count();
+    std::cout<< file_name_ <<" is copied, file size is " << (file_size_ / 1024) << " KB. Elapsed time: " << elapsed_time <<  " seconds" << std::endl;
+}
+
+void QCopyWorker::run()
+{
+    in_file_.open(path_, std::ios_base::binary);
+    dist_path_.append(file_name_);
+    out_file_.open(dist_path_, std::ios_base::binary);
+
+    t_st_point_ = std::chrono::high_resolution_clock::now();
+
+    while (!in_file_.eof())
+    {
+        in_file_.read(buffer_, BUFFER_SIZE);
+        bs_ = in_file_.gcount();
+        file_size_ += bs_;
+        out_file_.write(buffer_, bs_);
+
+    }
+
+    t_end_point_ = std::chrono::high_resolution_clock::now();
 
 }
